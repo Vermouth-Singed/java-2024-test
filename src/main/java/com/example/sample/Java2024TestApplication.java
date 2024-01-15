8456210ab3472ee08b991ed4b95b1b6358abc00d
Vermouth <qhddusl2004@naver.com>
2024-01-15 23:21:36 +0900

    Initial commit

---
+++ src/main/java/com/example/sample/Java2024TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2024TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2024TestApplication.class, args);
+	}
+
+}
